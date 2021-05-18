3bb25cf3932546b57a6170e130608642a74118c5
JulienPascal <julien.pascal@sciencespo.fr>
2021-05-18 16:35:06 +0200

    update to julia.1.6

--- README.md
+++ README.md
@@ -1,37 +1,39 @@
-# Discussion: An MCMC Approach to Classical Estimation
-This repository illustrates how to use LTEs (Laplace type estimators) to identify the coeffients of a median regression using the LAD estimator (Powell, 1984). LTEs were developped by V. Chernozhukov and H. Hong in "An MCMC approach to classical estimation", Journal of Econometrics, Vol. 115, N°2, pp. 293-346, 2003.
+# An MCMC Approach to Classical Estimation
 
-This code is still in its preliminary stage. One main problem is that the rejection rate is too high. A multivariate transition kernel is used, and the selection of the "jumping" factor is done manually so far. An adaptive MCMC scheme will be implemented in the future.
+This repository illustrates how to use **Laplace type estimators** (LTEs) in the
+context of a **median regression**. See the [slides.](https://github.com/JulienPascal/MCMC_Approach_Classical_Estimation/blob/master/main.pdf) for more details.
 
 ## The Problem at hand
-The data is generated according to: y_star = Xθ + u. Yet, only y = max(0,y_star) is observable to the econometrician. 
+The data is generated according to
+
+$$ y* = Xθ + u $$
+
+But we only observe
+
+$$ y = max(0, y*) $$
+
+Can we estimate $θ$ ?
 
 ## Example
-This scripts generate y_star and y for a sample of 1000 individuals such that the truncation rate is appoximately 40%.
+The script `main.jl` generates $y*$ and $y$ for a sample of 1000. Approximately 40% of the data is truncated. The true value for θ is set to $(1, 2, 3, 4)$
 
 ![GitHub Logo](https://github.com/JulienPascal/MCMC_Approach_Classical_Estimation/blob/master/output/censoring.png)
 
 
 ## Results
-The parameter b can estimated using the MCMC algorithm described by V. Chernozhukov and H. Hong (2003). In this example, the true value for θ is (1 3 3 3). The median of the quasi-posterior distribution can be used as an estimator of θ. Quantiles of the quasi-posterior distribution can also be used to build confidence intervals. 
 
-![GitHub Logo](https://github.com/JulienPascal/MCMC_Approach_Classical_Estimation/blob/master/output/draws_censored_regression.png)
+![GitHub Logo](https://github.com/JulienPascal/MCMC_Approach_Classical_Estimation/blob/master/output/chains.png)
 
-![GitHub Logo](https://github.com/JulienPascal/MCMC_Approach_Classical_Estimation/blob/master/output/histograms_censored_regression.png)
+![GitHub Logo](https://github.com/JulienPascal/MCMC_Approach_Classical_Estimation/blob/master/output/histograms.png)
 
-|           |                     |                     |                     |                    | 
-|-----------|---------------------|---------------------|---------------------|--------------------| 
-| variable  |  θ0                 |  θ1                 |  θ2                 |  θ3                | 
-| P10       | 0.9729222463036142  | 2.823425986798605   | 2.9694823610212064  | 2.9817850591413575 | 
-| Median    | 1.0000193577408492  | 2.886062386934549   | 2.9972225679108764  | 3.0101184734177098 | 
-| P90       | 1.0291370017480144  | 2.9474441003672367  | 3.02585751230858    | 3.041841506192087  | 
-Note: The true value for  θ = (θ1  θ2  θ3  θ4) is (1 3 3 3). This table reports the 10th, the median and te 90th percentiles of the quasi-posterior distribution.
+| variable   | beta0    | beta1    | beta2    | beta3    |
+|------------|----------|----------|----------|----------|
+| P10        | 0.972387 | 1.920641 | 2.966820 | 3.975281 |
+| Median     | 1.002964 | 1.974698 | 2.994887 | 4.003844 |
+| P90        | 1.034758 | 2.031147 | 3.024863 | 4.034115 |
+| True value | 1        | 2        | 3        | 4        |
+Note: This table reports the 10th, the median and te 90th percentiles of the quasi-posterior distribution.
 
-## Presentation 
-More on MCMC and LTEs:
-[Read my presentation at Sciences Po](https://github.com/JulienPascal/MCMC_Approach_Classical_Estimation/blob/master/main.pdf)
-
-## Requirements
-Julia Version 0.4.5
-
-
+## References
+* J. L. Powell (1984), "Least Absolute Deviations estimation for the censored regression model", Journal of Econometrics 25, 303-325
+* V. Chernozhukov and H. Hong in "An MCMC approach to classical estimation", Journal of Econometrics, Vol. 115, N°2, pp. 293-346, 2003.
--- main.jl
+++ main.jl
@@ -1,337 +1,213 @@
-###################################################################################
+#------------------------------------------------------------------------------
 # LTE Estimation of the Least Absolute Deviations for the Censored Regression model
-###################################################################################
+#------------------------------------------------------------------------------
 # Julien Pascal
+# https://julienpascal.github.io/
 #
 # based on:
 # - J. L. Powell (1984), "Least Absolute Deviations estimation for the censored regression model", Journal of Econometrics 25, 303-325
 # - V. Chernozhukov and H. Hong (2003) "An MCMC approach to classical estimation" Journal of Econometrics 115, 293 - 346
 #
-#
 # This script: Estimate the LAD for the censored regression model with a LTE approach
-#
-# To do: Finish the adaptive MCMC algorithm
-#
-using PyPlot
-using Distributions
-using LaTeXStrings
-using Distributions
+#------------------------------------------------------------------------------
+using Distributed
+using RobustPmap
+using Test
+using Random
+using Statistics
+using Plots
+
+using CSV
 using GLM
 using DataFrames
 
-srand(123)
+addprocs(3)
+@everywhere using AffineInvariantMCMC
+@everywhere using Distributions
+@everywhere using LinearAlgebra
+#------------------------------------------------------------------------------
+# Paths
+#------------------------------------------------------------------------------
+path_to_main = pwd()
 
-#######################
-# path to main.jl file
-#######################
-# You may want to adapt this path to you setting:
-path_to_main = "/home/julien/MCMC_Approach_Classical_Estimation"
-
-include(string(path_to_main,"/src/censored_regression.jl"))
-include(string(path_to_main,"/src/Metropolis.jl"))
-
-######################################
-# Decide what type of algorithm to use
-######################################
-algo_type = "metropolis"
-#algo_type = "adaptive_metropolis"
-
-################
+#------------------------------------------------------------------------------
 # Generate data
-###############
-# true parameter
+#------------------------------------------------------------------------------
+# true parameters
 # produces approximately 40% of censoring rate:
-beta = transpose([1 3 3 3]) #beta as a column vector
-number_regressors = Int(size(beta)[1])
+beta = transpose([1 2 3 4]) #beta as a column vector
+number_regressors = size(beta,1)
 M = 1000 #number of people in the sample
 
 #Generate a 3*M matrix of observations:
-X = transpose(rand(MvNormal(zeros(3), eye(3)), M)) #transpose to have the usual matrix representation
+d = MvNormal(zeros(3), I(3))
+X = transpose(rand(d, M)) #transpose to have the usual matrix representation
 
 #Add a first column of one for the constant term
 X = hcat(ones(M),X)
-
 #Generate error terms:
 u = rand(Normal(0,1), M).*((X[:, 2]).^2)
-#u = rand(Normal(0,1), M).*abs(X[:, 2])
-
 #Generate Y star:
-Y_star = X*beta[1:number_regressors] + u
-
+Y_star = X*beta .+ u
 #Generate truncation:
-Y = max(0, Y_star)
+Y = max.(0.0, Y_star)
 
 #Censoring rate:
-println("Censoring rate = $(sum(Y .== 0)/M)")
+println("Censoring rate = $(100*sum(Y .== 0)/M) %")
 
-fig = figure("distribution of Y star",figsize=(10,10))
-subplot(121)
-title("distribution of Y star")
-h1 = plt[:hist](Y_star, 30, normed="True", color="blue") # Histogram
-
-subplot(122)
-title("distribution of Y")
-h2 = plt[:hist](Y, 30,  normed="True", color = "blue") # Histogram
-savefig(string(path_to_main,"/output/censoring.png"))
-
-#################
-# MCMC parameter:
-#################
-
-# # 1-D Search
-# sigma = 0.5 #"jump" parameter for the proposal matrix.
-
-# Multidimensional
-sigma = 0.15*eye(number_regressors) #"jump" parameter for the proposal matrix.
-
-#A = rand(number_regressors,number_regressors)
-#sigma = 0.001*(transpose(A)*A + eye(number_regressors))
+h1 = histogram(Y_star) # Histogram
+title!(h1, "Non-Truncated Distribution", legend=:none)
+h2 = histogram(Y) # Histogram
+title!(h2, "Truncated Distribution", legend=:none)
+h = plot(h1, h2)
+savefig(h, joinpath(path_to_main,"output/censoring.png"))
 
 
-N = 10000000 #number of times to iterate the markov chain
-burnin = Int((1/10)*N)
-##########################
-# initial draw for MCMC is
-##########################
-# Use the OLS estimates
-data = convert(DataFrame, hcat(Y,X))
-# Update names:
-rename!(data, {:x1=>:y})
-rename!(data, {:x2=>:x0})
-rename!(data, {:x3=>:x1})
-rename!(data, {:x4=>:x2})
-rename!(data, {:x5=>:x3})
-OLS = glm(y ~ x1 + x2 + x3, data, Normal(), IdentityLink())
+#-------------------------------------------------------------------------------
+# MCMC Estimation
+#-------------------------------------------------------------------------------
+@everywhere begin
+	numdims = 4
+	numwalkers = 100
+	thinning = 10
+	numsamples_perwalker = 1000
+	burnin = Int((1/10)*numsamples_perwalker)
+	lb = 0 .* ones(numdims) #lower bound
+	ub = 5 .* ones(numdims) #upper bound
+	# Uniform prior
+	d_prior = Product(Uniform.(lb, ub))
+	# Normal
+	# d_prior = MvNormal(zeros(numdims), 0.1 .* I(numdims))
+end
 
-#OLS = glm(x1 ~ x2 + x3 + x4,  convert(DataFrame, hcat(Y_star,X)), Normal(), IdentityLink())
+# Log-likelihood
+@everywhere function Ln(theta, Y, X)
+	Ln_theta = - sum(abs.(Y .- max.(0.0, X*theta)))
+	return Ln_theta
+end
 
-#intialize theta:
-theta = zeros(number_regressors, N)
+# Log quasi-posterior: Log(likelihood) + log(prior)
+@everywhere function quasi_posterior(theta, Y, X, d_prior)
+	 return Ln(theta, Y, X) + log(pdf(d_prior, theta))
+end
+
+# initial draw for MCMC is OLS
+data = DataFrame(y = Y[:,1], x1 = X[:,2], x2 = X[:,3], x3 = X[:,4])
+OLS = glm(@formula(y ~ x1 + x2 + x3), data, Normal(), IdentityLink())
 theta0 = coef(OLS)
 
-# 1-D Search
-# true value, except for beta1 which takes value from the OLS coeff:
-#theta[1:4,1] = [beta[1]; theta0[2]; beta[3]; beta[4]] #initial value of the markov chain
+# Option 1: slightly perturb the initial draws for the walkers
+x0 = ones(numdims, numwalkers).*theta0 .+ rand(numdims, numwalkers) .* 1.0
+# Option 2: introduce a bias to see how fast the chains converge
+#x0 = ones(numdims, numwalkers).*theta0 .+ ones(numdims, numwalkers) .* 1.0
 
-# Multidimensional Search
-# Initialize the first draw using OLS coefficients:
-theta[1:4,1] = coef(OLS)
+# See https://github.com/madsjulia/AffineInvariantMCMC.jl
+#=
+chain, llhoodvals = AffineInvariantMCMC.sample(x -> Ln(x, Y, X), numwalkers, x0, burnin, 1)
+chain, llhoodvals = AffineInvariantMCMC.sample(x -> Ln(x, Y, X), numwalkers, chain[:, :, end], numsamples_perwalker, thinning)
+flatchain, flatllhoodvals = AffineInvariantMCMC.flattenmcmcarray(chain, llhoodvals)
+=#
 
-# Define the parameter space:
-theta_upper_limit = theta[1:4,1] + 10
-theta_lower_limit = theta[1:4,1] - 10
+chain, llhoodvals = AffineInvariantMCMC.sample(x -> quasi_posterior(x, Y, X, d_prior), numwalkers, x0, burnin, 1)
+chain, llhoodvals = AffineInvariantMCMC.sample(x -> quasi_posterior(x, Y, X, d_prior), numwalkers, chain[:, :, end], numsamples_perwalker, thinning)
+flatchain, flatllhoodvals = AffineInvariantMCMC.flattenmcmcarray(chain, llhoodvals)
 
-# To keep track of acceptance and rejection rates:
-Accept = 0
-Reject = 0
+#-------------------------------------------------------------------------------
+# Plot Draws
+#-------------------------------------------------------------------------------
+#Use the function "squeeze" to go from Array{Float64,2} to Array{Float64,1}: necessary for plotting
+p1 = plot(flatchain[1,:], ylabel="theta0", xlabel="T", legend=:none)
+p2 = plot(flatchain[2,:], ylabel="theta1", xlabel="T", legend=:none)
+p3 = plot(flatchain[3,:], ylabel="theta2", xlabel="T", legend=:none)
+p4 = plot(flatchain[4,:], ylabel="theta3", xlabel="T", legend=:none)
+p5 = plot(p1, p2, p3, p4)
+savefig(p5, joinpath(path_to_main,"output/chains.png"))
+display(p5)
 
-if algo_type == "metropolis"
+#-------------------------------------------------------------------------------
+# Plot Histograms
+#-------------------------------------------------------------------------------
+hh1 = histogram(flatchain[1,burnin:end], title="theta0", legend=:none)
+hh2 = histogram(flatchain[2,burnin:end], title="theta1", legend=:none)
+hh3 = histogram(flatchain[3,burnin:end], title="theta2", legend=:none)
+hh4 = histogram(flatchain[4,burnin:end], title="theta3", legend=:none)
+hh5 = plot(hh1, hh2, hh3, hh4)
+savefig(hh5, joinpath(path_to_main,"output/histograms.png"))
+display(hh5)
 
-  #############################
-  # Simple Metropolis Algorithm
-  #############################   
-  for m in 2:N
-    #sample proposed from a symmetric jumping distribution:
+#-------------------------------------------------------------------------------
+# Use the sample to find quasi-posterior summary statistics
+#-------------------------------------------------------------------------------
+# append true value as well
+result_beta0 = append!(quantile(flatchain[1,burnin:end],[0.10, 0.5, 0.90]), beta[1])
+result_beta1 = append!(quantile(flatchain[2,burnin:end],[0.10, 0.5, 0.90]), beta[2])
+result_beta2 = append!(quantile(flatchain[3,burnin:end],[0.10, 0.5, 0.90]), beta[3])
+result_beta3 = append!(quantile(flatchain[4,burnin:end],[0.10, 0.5, 0.90]), beta[4])
+results = DataFrame(variable = ["P10"; "Median"; "P90"; "True value"], beta0 = result_beta0,
+						beta1 = result_beta1, beta2 = result_beta2, beta3 = result_beta3)
 
-    # Multidimensional Search
-    theta_trial = generate_proposal(theta[1:4, m-1], 100, sigma, "normal")
+CSV.write(joinpath(path_to_main,"output/output_table.csv"), results)
 
-    # 1-D Search
-    #theta0_trial = generate_proposal(theta[2, m-1], 100, sigma, "normal")
-    
-
-    #draw from a uniform on [0,1]:
-    u = rand(Uniform(0,1))
-
-    if u < (exp(Ln(theta_trial, Y, X, M))*prior(theta_trial, theta_upper_limit, theta_lower_limit, "normal")/(exp(Ln(theta[1:4, m-1], Y, X, M))*prior(theta[1:4, m-1], theta_upper_limit, theta_lower_limit, "normal")))
-      #accept the new draw
-      Accept = Accept + 1
-      theta[1:4, m] = theta_trial
-    else
-      #keep the previous draw
-      Reject = Reject + 1
-      theta[1:4, m] = theta[1:4, m-1]
-    end
-
-  end
-##########################################
-elseif algo_type == "adaptive_metropolis"
-  #########################################
-  # Adaptive MCMC with vanishing adaptation
-  #########################################
-  #########################################
-  # WORK IN PROGRESS : the algorith is not 
-  # operational so far
-  #########################################
-  # based on "Adaptive Markov chain Monte Carlo sampling
-  # and estimation in Mata", J Baker 
-
-  # Initialization of extra parameters (follow the paper):
-  # the initial draw (X0) was already set above
-  mu_0 = theta0 + 1 #need to initialize in a way. 
-  Sigma_0 = 1.0*eye(number_regressors) 
-  #
-  #A = rand(number_regressors,number_regressors)
-  #Sigma_0 = 0.1*(transpose(A)*A + eye(number_regressors))
-
-  d = number_regressors
-  lambda_0 = (2.38^2)/d #recommended initial scaling parameter (according to the paper quoted above)
-
-  alpha_star = 0.234 #optimal acceptance rate
-  delta = 2/3
-
-  # Initialize vectors in which I am storing the "tuning" parameters as the iteration moves forward:
-  mu_t = zeros(number_regressors, N)*NaN
-  lambda_t = zeros(N)*NaN
-  gamma_t = zeros(N)*NaN
-  Sigma_t = zeros(number_regressors,number_regressors,N)*NaN #mutlidimensional array 
-  Var_covariance_proposal_t = zeros(number_regressors,number_regressors,N)*NaN #mutlidimensional array 
-
-  # Store the initial values:
-  mu_t[:, 1] = mu_0
-  lambda_t[1] = lambda_0
-  gamma_t[1] = 1/((1+1)^delta)
-  Sigma_t[:,:,1] = Sigma_0
-  Var_covariance_proposal_t[:,:,1] = lambda_0*Sigma_0
-
-  tol = 0.01
-
-  for m in 2:N
-
-    # Draw a candidate: 
-    # Multidimensional Search:
-    theta_trial = generate_proposal(theta[1:4, m-1], 100, Var_covariance_proposal_t[:,:,(m-1)], "normal")
-
-    # Calculate the "pseudo-likelihood" ratio:
-    pseudo_likelihood_ratio = (exp(Ln(theta_trial, Y, X, M))*prior(theta_trial, theta_upper_limit, theta_lower_limit, "normal")/(exp(Ln(theta[1:4, m-1], Y, X, M))*prior(theta[1:4, m-1], theta_upper_limit, theta_lower_limit, "normal")))
-    
-    # will be useful for later steps:
-    # have to take care of NaN that can occur (division by zero):
-    if isnan(pseudo_likelihood_ratio) == true
-      alpha_Y_X = 0
-    elseif pseudo_likelihood_ratio > 1 
-      alpha_Y_X  = 1
-    else 
-      alpha_Y_X = pseudo_likelihood_ratio
-    end
-    
-
-    #draw from a uniform on [0,1]:
-    u = rand(Uniform(0,1))
-
-    if u < alpha_Y_X 
-      #accept the new draw
-      Accept = Accept + 1
-      theta[1:4, m] = theta_trial
-    else
-      #keep the previous draw
-      Reject = Reject + 1
-      theta[1:4, m] = theta[1:4, m-1]
-    end
-
-    # [Step 6 in the paper] Compute weighting parameter gamma_t:
-    gamma_t[m-1] = 1/((1+(m-1))^delta)
-
-    # when gamma_t is small enough, stop the adujstement phase:
-    if gamma_t[m-1] > tol
-      # [Step 7 in the paper] update  lambda_t_+1:
-      lambda_t[m] = exp(gamma_t[m-1]*(alpha_Y_X - alpha_star))*lambda_t[m-1]
-
-      # [Step 8 in the paper] update mu_t_+1:
-      #precalculate an element:
-      A = theta[1:4, m] - mu_t[:,(m-1)]
-      mu_t[:,m] = mu_t[:,(m-1)] + gamma_t[m-1]*(A)
-
-      # [Step 9 in the paper] update Sigma_t_+1:
-      #precalculate an element:
-      B = A*transpose(A) - Sigma_t[:,:,(m-1)]
-      if isnan(gamma_t[m-1]*(B)) == false #when gamma_t goes to zero, there is a moment when it appears as "NaN"
-        Sigma_t[:,:,m] = Sigma_t[:,:,(m-1)] + gamma_t[m-1]*(B)
-      else
-        Sigma_t[:,:,m] = Sigma_t[:,:,(m-1)]
-      end
-      # [Extra step not explicit in the paper] update the variance-covariance for the proposal distribution:
-      Var_covariance_proposal_t[:,:,m]  = lambda_t[m]*Sigma_t[:,:,m]
-    else # when the adjustement phase is over, keep the previous variance-covariance:
-      Var_covariance_proposal_t[:,:,m]  = Var_covariance_proposal_t[:,:,(m-1)]
-    end
-    
-    
-  end
-#################
-else 
-  error("Specify a correct algorithm type")
+#-------------------------------------------------------------------------------
+# Example with GMM
+#-------------------------------------------------------------------------------
+# Let's assume now that we observe Y_star
+@everywhere begin
+	numdims = 4
+	numwalkers = 100
+	thinning = 10
+	numsamples_perwalker = 2000
+	burnin = Int((1/10)*numsamples_perwalker)
+	lb = 0 .* ones(numdims) #lower bound
+	ub = 5 .* ones(numdims) #upper bound
+	# Uniform prior
+	d_prior = Product(Uniform.(lb, ub))
 end
 
 
-############
+# Log-likelihood
+@everywhere function Ln_GMM(theta, Y, X)
+	m = (1/sqrt(size(X,1))).*(transpose(X)*(Y .- X*theta))
+	Ln_theta = - 0.5*transpose(m)*I(size(theta,1))*m
+	return Ln_theta[1]
+end
+
+chain, llhoodvals = AffineInvariantMCMC.sample(x -> Ln_GMM(x, Y_star, X), numwalkers, x0, burnin, 1)
+chain, llhoodvals = AffineInvariantMCMC.sample(x -> Ln_GMM(x, Y_star, X), numwalkers, chain[:, :, end], numsamples_perwalker, thinning)
+flatchain, flatllhoodvals = AffineInvariantMCMC.flattenmcmcarray(chain, llhoodvals)
+
+#-------------------------------------------------------------------------------
 # Plot Draws
-############
-fig = figure("Draws of theta",figsize=(10,10))
-subplot(221)
-title("theta0")
+#-------------------------------------------------------------------------------
 #Use the function "squeeze" to go from Array{Float64,2} to Array{Float64,1}: necessary for plotting
-plot(1:N, squeeze(theta[1,:], 1), color=:grey, alpha=0.5)
-ylabel("theta")
-xlabel("period")
+p1 = plot(flatchain[1,:], ylabel="theta0", xlabel="T", legend=:none)
+p2 = plot(flatchain[2,:], ylabel="theta1", xlabel="T", legend=:none)
+p3 = plot(flatchain[3,:], ylabel="theta2", xlabel="T", legend=:none)
+p4 = plot(flatchain[4,:], ylabel="theta3", xlabel="T", legend=:none)
+p5 = plot(p1, p2, p3, p4)
+savefig(p5, joinpath(path_to_main,"output/chains_GMM.png"))
+display(p5)
 
 
-subplot(222)
-title("theta1")
-plot(1:N, squeeze(theta[2,:],1), color=:grey, alpha=0.5)
-ylabel("theta")
-xlabel("period")
+hh1 = histogram(flatchain[1,burnin:end], title="theta0", legend=:none)
+hh2 = histogram(flatchain[2,burnin:end], title="theta1", legend=:none)
+hh3 = histogram(flatchain[3,burnin:end], title="theta2", legend=:none)
+hh4 = histogram(flatchain[4,burnin:end], title="theta3", legend=:none)
+hh5 = plot(hh1, hh2, hh3, hh4)
+savefig(hh5, joinpath(path_to_main,"output/histograms_GMM.png"))
+display(hh5)
 
-subplot(223)
-title("theta2")
-plot(1:N, squeeze(theta[3,:],1), color=:grey, alpha=0.5)
-ylabel("theta")
-xlabel("period")
+# Compare to traditional OLS
+data = DataFrame(y = Y_star[:,1], x1 = X[:,2], x2 = X[:,3], x3 = X[:,4])
+GMM = glm(@formula(y ~ x1 + x2 + x3), data, Normal(), IdentityLink())
+stds_GMM = stderror(GMM)
+coefs_GMM = coef(GMM)
 
-subplot(224)
-title("theta3")
-plot(1:N, squeeze(theta[4,:],1), color=:grey, alpha=0.5)
-ylabel("theta")
-xlabel("period")
-savefig(string(path_to_main,"/output/draws_censored_regression.png"))
+result_beta0 = append!(quantile(flatchain[1,burnin:end],[0.05, 0.10, 0.5, 0.90, 0.95]), std(flatchain[1,burnin:end]), beta[1])
+result_beta1 = append!(quantile(flatchain[2,burnin:end],[0.05, 0.10, 0.5, 0.90, 0.95]), std(flatchain[2,burnin:end]), beta[2])
+result_beta2 = append!(quantile(flatchain[3,burnin:end],[0.05, 0.10, 0.5, 0.90, 0.95]), std(flatchain[3,burnin:end]), beta[3])
+result_beta3 = append!(quantile(flatchain[4,burnin:end],[0.05, 0.10, 0.5, 0.90, 0.95]), std(flatchain[4,burnin:end]), beta[4])
+results = DataFrame(variable = ["P5"; "P10"; "Median"; "P90"; "P95"; "std"; "True value"], beta0 = result_beta0,
+						beta1 = result_beta1, beta2 = result_beta2, beta3 = result_beta3)
 
-#################
-# Plot Histograms
-#################
-fig = figure("Histogram for theta",figsize=(10,10))
-subplot(221)
-title("theta0")
-#Use the function "squeeze" to go from Array{Float64,2} to Array{Float64,1}: necessary for plotting
-h0 = plt[:hist](squeeze(theta[1, burnin:N],1), 50, normed="True", color="blue") # Histogram
-
-subplot(222)
-title("theta1")
-h1 = plt[:hist](squeeze(theta[2, burnin:N],1), 50, normed="True", color="blue") # Histogram
-
-
-subplot(223)
-title("theta2")
-h2 = plt[:hist](squeeze(theta[3, burnin:N],1), 50, normed="True", color="blue") # Histogram
-
-subplot(224)
-h3 = plt[:hist](squeeze(theta[4, burnin:N],1), 50, normed="True", color="blue") # Histogram
-title("theta3")
-savefig(string(path_to_main,"/output/histograms_censored_regression.png"))
-
-################################
-# Use the sample to find moments
-################################
-result_beta0 = quantile(squeeze(theta[1, burnin:N],1),[0.10, 0.5, 0.90])
-result_beta1 = quantile(squeeze(theta[2, burnin:N],1),[0.10, 0.5, 0.90])
-result_beta2 = quantile(squeeze(theta[3, burnin:N],1),[0.10, 0.5, 0.90])
-result_beta3 = quantile(squeeze(theta[4, burnin:N],1),[0.10, 0.5, 0.90])
-
-results = DataFrame(variable = ["P10"; "Median"; "P90"], beta0 = result_beta0, beta1 = result_beta1, beta2 = result_beta2, beta3 = result_beta3)
-writetable(string(path_to_main,"/output/output_table.csv"), results)
-
-
-println("Rejection rate = $(Reject/N)")
-println("Acception rate = $(Accept/N)")
-
-
-
+CSV.write(joinpath(path_to_main,"output/output_table_GMM.csv"), results)
